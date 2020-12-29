6d48bb8dc525a5b07154dc8e02b74676ff6ab436
Serhat94 <serhat.yaren0935@gmail.com>
2020-12-30 01:34:06 +0300

    Kaydet butonunu aktif hale getirmek için veritabanımız ile baglantı oluşturdum

--- WinFormKOS/FormKitapEkle.cs
+++ WinFormKOS/FormKitapEkle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -24,8 +25,8 @@ namespace WinFormKOS
         }
         void comboBoxFill()
         {
-            foreach(DataRow row in IDataBase.DataToDataTable("Select * From dolaplar").Rows)
-            
+            foreach (DataRow row in IDataBase.DataToDataTable("Select * From dolaplar").Rows)
+
                 cbbDolap.Items.Add(row["adi"].ToString());
 
             foreach (DataRow row in IDataBase.DataToDataTable("Select * From turler").Rows)
@@ -39,7 +40,25 @@ namespace WinFormKOS
             foreach (DataRow row in IDataBase.DataToDataTable("Select * From yazarlar").Rows)
 
                 cbbYazarAdi.Items.Add(row["adi"].ToString());
+        }
+            void kitapEkle()
+            {
+                List<SqlParameter> parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter("@kayitNo", SqlDbType.Int) { Value = txtKayitNo.Text });
+                parameters.Add(new SqlParameter("@kitapAdi", SqlDbType.VarChar) { Value = txtKitapAdi.Text });
+                parameters.Add(new SqlParameter("@yazarAdi", SqlDbType.VarChar) { Value = cbbYazarAdi.Text });
+                parameters.Add(new SqlParameter("@yayinevi", SqlDbType.VarChar) { Value = cbbYayınevi.Text });
+                parameters.Add(new SqlParameter("@basimyili", SqlDbType.VarChar) { Value = txtBasimYil.Text });
+                parameters.Add(new SqlParameter("@sayfaSayisi", SqlDbType.VarChar) { Value = txtSayfaSayisi.Text });
+                parameters.Add(new SqlParameter("@tur", SqlDbType.VarChar) { Value = cbbTur.Text });
+                parameters.Add(new SqlParameter("@aciklama", SqlDbType.VarChar) { Value = txtAciklama.Text });
+                parameters.Add(new SqlParameter("@dolap", SqlDbType.VarChar) { Value = cbbDolap.Text });
+                parameters.Add(new SqlParameter("@raf", SqlDbType.VarChar) { Value = txtRaf.Text });
+                parameters.Add(new SqlParameter("@sira", SqlDbType.VarChar) { Value = txtSira.Text });
 
+               
+
+            }
         }
     }
-}
+
